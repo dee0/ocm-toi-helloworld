5762c6f772814e1ace5a974a528cb400d0c53496
d.small@sap.com <d.small@sap.com>
2024-12-25 01:26:45 +0000

    Update simulation script so that it also simulates step #5 of the toi install process

--- scripts/simulateTOIWorkflowWithSpiffCLI.sh
+++ scripts/simulateTOIWorkflowWithSpiffCLI.sh
@@ -11,7 +11,8 @@ trap printError EXIT
 
 set -xe -o pipefail 
 
-ROOT=./tmp/test/toi
+TMP=./tmp
+ROOT=${TMP}/test/toi
 INPUTS=${ROOT}/inputs
 
 rm -rf $ROOT || :
@@ -20,6 +21,8 @@ mkdir -p $INPUTS || :
 # Perform step #2 of the TOI process 
 spiff++ merge ./toiExecutor/toiExecutorConfig.yaml ./toiExecutor/toiPackageExecutorConfig.yaml ./toiExecutor/toiExecutorTemplateLibrary1.yaml ./toiExecutor/toiExecutorTemplateLibrary2.yaml |& tee ${INPUTS}/config
 
+# Perform step #5 of the TOI process 
+spiff++ merge ./toiPackage/toiPackageConfigTemplate.yaml ./userInput/parametersFromCLI.yaml ./toiPackage/toiPackageTemplateLibrary1.yaml ./toiPackage/toiPackageTemplateLibrary2.yaml ./toiPackage/getCredentials.yaml |& tee ${TMP}/ocm-toi-step5-output.yaml
 
 
 
