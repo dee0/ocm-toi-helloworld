9b6cd9bb0d8ddb4ec46ec31f38f5fa3dd809c93f
d.small@sap.com <d.small@sap.com>
2024-12-26 21:46:23 +0000

    Re-org files to better match reality

--- scripts/simulateTOIWorkflowWithSpiffCLI.sh
+++ scripts/simulateTOIWorkflowWithSpiffCLI.sh
@@ -19,7 +19,7 @@ rm -rf $ROOT || :
 mkdir -p $INPUTS || :
 
 # Perform step #2 of the TOI process 
-spiff++ merge ./toiExecutor/toiExecutorConfig.yaml ./toiExecutor/toiPackageExecutorConfig.yaml ./toiExecutor/toiExecutorTemplateLibrary1.yaml ./toiExecutor/toiExecutorTemplateLibrary2.yaml |& tee ${INPUTS}/config
+spiff++ merge ./toiExecutor/toiExecutorConfigTemplate.yaml ./toiPackage/toiPackageExecutorConfig.yaml ./toiExecutor/toiExecutorTemplateLibrary1.yaml ./toiExecutor/toiExecutorTemplateLibrary2.yaml |& tee ${INPUTS}/config
 
 # Perform step #5 of the TOI process 
 spiff++ merge ./toiPackage/toiPackageConfigTemplate.yaml ./userInput/parametersFromCLI.yaml ./toiPackage/toiPackageTemplateLibrary1.yaml ./toiPackage/toiPackageTemplateLibrary2.yaml ./toiPackage/getCredentials.yaml |& tee ${TMP}/ocm-toi-step5-output.yaml
