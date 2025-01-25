1e44f1530446961ddf1f552835e3afb627e3e6af
d.small@sap.com <d.small@sap.com>
2025-01-25 19:18:01 +0000

    Correct library usage

--- examples/complex/scripts/simulateTOIWorkflowWithSpiffCLI.sh
+++ examples/complex/scripts/simulateTOIWorkflowWithSpiffCLI.sh
@@ -18,19 +18,34 @@ INPUTS=${ROOT}/inputs
 rm -rf $ROOT || :
 mkdir -p $INPUTS || :
 
-# Perform step #2 of the TOI process 
+: ======================
+: Perform step 2 of the TOI process 
+spiff++ --features control merge <(yq '.executors[0].config' ./toiPackage/toiPackageOne.yaml) \
+  ./toiExecutor/toiExecutorTemplateLibraryOne.yaml \
+  ./toiExecutor/toiExecutorTemplateLibraryTwo.yaml |& tee ${INPUTS}/ocm-toi-step2-output.yaml
+
+: ======================
+: Perform step 3 of the TOI process 
 spiff++ --features control merge <(yq '.configTemplate'  ./toiExecutor/toiExecutorOne.yaml) \
-  <(yq '.executors[0].config' ./toiPackage/toiPackageOne.yaml) \
+  ${INPUTS}/ocm-toi-step2-output.yaml \
   ./toiExecutor/toiExecutorTemplateLibraryOne.yaml \
   ./toiExecutor/toiExecutorTemplateLibraryTwo.yaml |& tee ${INPUTS}/config
 
-# Perform step #5 of the TOI process 
+: ======================
+: Perform step 6 of the TOI process 
+spiff++ --features control merge ./userInput/parametersFromCLI.yaml \
+  ./toiPackage/toiPackageTemplateLibraryOne.yaml \
+  ./toiPackage/toiPackageTemplateLibraryTwo.yaml |& tee ${TMP}/ocm-toi-step6-output.yaml
+
+: ======================
+: Perform step 7 of the TOI process 
 spiff++ --features control merge <(yq '.configTemplate' toiPackage/toiPackageOne.yaml) \
-  ./userInput/parametersFromCLI.yaml \
+  ${TMP}/ocm-toi-step6-output.yaml \
   ./toiPackage/toiPackageTemplateLibraryOne.yaml \
   ./toiPackage/toiPackageTemplateLibraryTwo.yaml |& tee ${TMP}/ocm-toi-step5-output.yaml
 
-# Perform step #6 of the TOI process 
+: ======================
+: Perform step 8 of the TOI process 
 spiff++ --features control merge <(yq '.executors[0].parameterMapping' toiPackage/toiPackageOne.yaml) \
   ${TMP}/ocm-toi-step5-output.yaml \
   ./toiPackage/getCredentials.yaml |& tee ${INPUTS}/parameters
